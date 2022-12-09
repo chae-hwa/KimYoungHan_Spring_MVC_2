b14407fbaa88e031429dcecf897236cd8ba7a8af
chae-hwa <ihj0519@naver.com>
2022-12-09 16:10:56 +0900

    세팅

---
+++ mvc2_1/src/main/java/hello/mvc2_1/Mvc21Application.java
@@ -0,0 +1,13 @@
+package hello.mvc2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc21Application.class, args);
+    }
+
+}
---
+++ mvc2_1/src/test/java/hello/mvc2_1/Mvc21ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mvc2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
