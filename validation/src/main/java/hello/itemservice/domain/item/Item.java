43c21275ff2528df28971905961fb1cfa7af938b
chae-hwa <ihj0519@naver.com>
2022-12-23 16:01:43 +0900

    BeanValidation - BeanValidation(에러 코드)

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,7 +11,12 @@ import javax.validation.constraints.NotNull;
 public class Item {
 
     private Long id;
-    @NotBlank
+    /** 메세지 찾는 순서
+     * 1. errors.properties에서 먼저 찾고
+     * 2. 없으면 어노테이션의 message 속성 적용
+     * 3. 또 없으면 라이브러리 기본 값 사용
+     */
+    @NotBlank(message = "공백 x")
     private String itemName;
     @NotNull
     @Range(min = 1000, max = 1000000)
