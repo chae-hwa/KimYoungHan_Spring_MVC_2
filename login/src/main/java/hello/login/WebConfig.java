b8cb48edcecb89b1baa2f62dc22f507db120f197
chae-hwa <ihj0519@naver.com>
2022-12-30 13:37:51 +0900

    로그인 처리2(필터, 인터셉터) - 서블릿 필터(인증 체크)

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,15 @@ public class WebConfig {
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*"); // 화이트 리스트에서 걸렀기 떄문에 모든 url 적용해도 괜찮음 (따로 지정하게 되면 여기서 하나하나 적용해야 돼서)
         return filterRegistrationBean;
     }
 }
--- login/src/main/java/hello/login/web/filter/LogFilter.java
+++ login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -14,7 +14,7 @@ public class LogFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
         log.info("log filter init");
     }
-    
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
